1805057c175746bbcc6e63e4e1ee723e70c2b3f5
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-16 12:56:26 +1100

    all work for polys

--- generators.jl
+++ generators.jl
@@ -1,5 +1,5 @@
 include("delta.jl")
-include("eis_series.jl")
+include("eisenstein.jl")
 
 #This file contains functions Delta16, Delta18, Delta20, Delta22, and Delta26 of the 
 #corresponding 1D spaces of cusp forms (S_k)
@@ -11,9 +11,9 @@ include("eis_series.jl")
 function Delta16(prec=10, var="q")
 
 	#cusp form of weight 16 to precision prec
-	delta_times_E4 = delta_qexp(prec, var)*eisenstein_series_qexp(4, prec)
+	delta_E4 = delta_qexp(prec, var)*eisenstein_series_poly(4, prec)
 
-	return (-8//bernoulli(4))*delta_times_E4
+	return (-8//bernoulli(4))*truncate(delta_E4, prec)
 end
 
 
@@ -21,9 +21,9 @@ end
 function Delta18(prec=10, var="q")
 
         #cusp form of weight 18 to precision prec
-        delta_times_E6 = delta_qexp(prec, var)*eisenstein_series_qexp(6, prec)
+        delta_E6 = delta_qexp(prec, var)*eisenstein_series_poly(6, prec)
 
-        return (-12//bernoulli(6))*delta_times_E6
+        return (-12//bernoulli(6))*truncate(delta_E6, prec)
 end
 
 
@@ -31,7 +31,33 @@ end
 function Delta20(prec=10, var="q")
 
         #cusp form of weight 20 to precision prec
-        delta_times_E4_twice = delta_qexp(prec, var)*(eisenstein_series_qexp(4, prec)^2)
+	E4_E4 = (eisenstein_series_poly(4, prec))^2
+        delta_E4_E4 = delta_qexp(prec, var)*truncate(E4_E4, prec)
 
-        return ((-8//bernoulli(4))^2)*delta_times_E4_twice		#must do //2?
+        return ((-8//bernoulli(4))^2)*truncate(delta_E4_E4, prec)
 end
+
+
+#Return the q expansion for Delta22 to precision prec
+function Delta22(prec=10, var="q")
+
+        #cusp form of weight 22 to precision prec
+	E4_E6 = eisenstein_series_poly(4, prec)*eisenstein_series_poly(6, prec)
+        delta_E4_E6 = delta_qexp(prec, var)*truncate(E4_E6, prec)
+
+        return (-8//bernoulli(4))*(-12//bernoulli(6))*truncate(delta_E4_E6, prec)
+end
+
+
+#Return the q expansion for Delta26 to precision prec
+function Delta26(prec=10, var="q")
+	
+	#cusp form of weight 26 to precision prec
+	E4_E4 = (eisenstein_series_poly(4, prec))^2
+	E4_E4_E6 = truncate(E4_E4, prec)*eisenstein_series_poly(6, prec)
+	delta_E4_E4_E6 = delta_qexp(prec, var)*truncate(E4_E4_E6, prec)
+
+	return ((-8//bernoulli(4))^2)*(-12//bernoulli(6))*truncate(delta_E4_E4_E6, prec)
+end
+
+

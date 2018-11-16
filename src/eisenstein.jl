153ad997fd59a8ac4d0b6d5b878a3df46a6b91a5
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-17 08:18:44 +1100

    updated eisenstein.jl s.t. better readable and added variable

--- src/eisenstein.jl
+++ src/eisenstein.jl
@@ -1,32 +1,28 @@
-#Return the q-expansion up to precision prec of the weight k Eisenstein series as a FLINT 
-#Fmpz_poly object, normalised such that coefficients are integers with no common factor 
-function eisenstein_series_poly(k, prec)
+#Return the q-expansion up to precision prec (default: 10) of the weight k Eisenstein 
+#series as a polynomial over QQ in the variable var (default: "q"), normalised such that 
+#the linear coefficient is 1 
+function eisenstein_series_poly(k, prec=10, var="q")
 
-	R, q = PolynomialRing(QQ, "q") 	#QQ as ring to prevent bug at the end
+	R, q = PolynomialRing(QQ, var) 	#QQ as ring to prevent bug at the end
 	qexp = R(0)
 
+	#error handling 
         if k%2 != 0 || k < 2
                 error("k must be an even positive integer")
         end
-
         if prec < 0
                 error("prec must be an even nonnegative integer")
         elseif prec == 0
                 return qexp
         end
-
 	
 	#initialise a0
 	a0 = - bernoulli(k) // 2k 	#bernoulli is fmpq so need //
-
-	#sum of sigma function times q^n
-	sigmasum = 0
-	for n in 2:prec-1
-		sigmasum += sigma(fmpz(n),k-1)*q^n
+	
+	qexp += a0
+	for n in 1:prec-1
+		qexp += sigma(fmpz(n),k-1)*q^n
 	end
 
-	qexp += a0 + q + sigmasum 		
-	
 	return qexp
-	
 end

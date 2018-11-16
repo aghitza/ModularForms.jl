9a6333efa15fcf7d3502ece8e1ebab5dfe722dbe
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-16 22:02:48 +1100

    include all existing functionalitites

--- src/ModularForms.jl
+++ src/ModularForms.jl
@@ -3,5 +3,10 @@ module ModularForms
 using Nemo
 
 include("delta.jl")
+include("eis_series.jl")
+include("eisenstein.jl")
+include("generators.jl")
+include("vm_basis.jl")
+include("hecke.jl")
 
 end

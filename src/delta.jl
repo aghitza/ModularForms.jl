935971ee3d16016f7c53f01274163b089acc28ca
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-17 05:18:31 +1100

    delta_poly and delta_qexp

--- src/delta.jl
+++ src/delta.jl
@@ -1,13 +1,30 @@
-#This file contains the delta function by using eta_qexp from Nemo
-#Need "using Nemo" to make it work
+include("big_oh.jl") 
+export delta_poly, delta_qexp
 
-export delta_qexp
+#This file contains the functions delta_poly and delta_qexp which return the q expansion 
+#of delta (the weight 12 level 1 cusp form) as a polynomial or powerseries (respectively).
+#The algorithm uses the function eta_qexp from Nemo. 
 
-#Return the q-expansion of the (normalised) cusp form of weight 12 (level 1) to 
-#precision prec (default=10) with coefficients in the ring ZZ. 
-function delta_qexp(prec=10, var="q")
-	R, q = PolynomialRing(ZZ, var)
+
+
+#Return the q-expansion of the (normalised) cusp form of weight 12 to precision prec 
+#(default: 10) as a polynomial over K (default: ZZ) in the variable var (default: "q"). 
+function delta_poly(prec=10, var="q", K=ZZ)
+
+	R, q = PolynomialRing(K, var)
 	delta = q*eta_qexp(24, prec-1, q) 
+
 	return delta
 end
 
+
+
+#Return the q-expansion of the (normalised) cusp form of weight 12 to precision prec 
+#(default: 10) as a power series over K (default: ZZ) in the variable var (default: "q").
+function delta_qexp(prec=10, var="q", K=ZZ) 
+	
+	delta = delta_poly(prec, var, K)
+	power_series = big_oh(delta, prec, var)
+
+	return power_series 
+end

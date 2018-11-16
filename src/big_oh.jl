6ba7b03025ac7a24fe28063e555c340b7121738a
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-17 05:18:04 +1100

    big_oh now ready for testing

--- src/big_oh.jl
+++ src/big_oh.jl
@@ -1,15 +1,16 @@
-#Construct a PowerSeries element to precision prec from a Polynomial element f, over the 
-#given ring (which must be the same as the ring corresponding to f)
-function big_oh(f, prec, K)				#add var
-	R, q = PowerSeriesRing(K, prec, "q")
+#Construct a relative power series to precision prec (default: 10) from a polynomial f 
+#over the same ring as f in the variable var (default: "q")
+function big_oh(f, prec=10, var="q")
+	K = base_ring(f)
+	R, q = PowerSeriesRing(K, prec, var)
 
 	power_series = R(0*q)
-	max_power = length(f) - 1 	#highest appearing power (OR must be prec?)
-	for i in 1:max_power
+	max_power = min(length(f), prec)
+	max_power = max_power - 1
+	for i in 0:max_power
 		coef = coeff(f,i)
 		power_series += R(coef*q^i)
 	end
 	
-	println(typeof(power_series))
 	return power_series
 end

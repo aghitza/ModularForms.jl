dc9306e1bf2f7c0a8f34f10b664b64c06f9b47de
lengelberts <lengelberts@student.unimelb.edu.au>
2018-11-17 07:53:50 +1100

    big_oh solved problem with f=0

--- src/big_oh.jl
+++ src/big_oh.jl
@@ -1,10 +1,16 @@
 #Construct a relative power series to precision prec (default: 10) from a polynomial f 
 #over the same ring as f in the variable var (default: "q")
 function big_oh(f, prec=10, var="q")
+
+	#initialise 
 	K = base_ring(f)
 	R, q = PowerSeriesRing(K, prec, var)
+	power_series = R(0)
 
-	power_series = R(0*q)
+	if f == 0
+		return power_series
+	end
+
 	max_power = min(length(f), prec)
 	max_power = max_power - 1
 	for i in 0:max_power

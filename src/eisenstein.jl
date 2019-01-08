f8f8c96887bd0edc754775d7df0e3d5d12877c43
lengelberts <lengelberts@student.unimelb.edu.au>
2019-01-08 12:45:22 +1100

    add normalization to eisenstein_series_poly; output in QQ if linear or constant to prevent errors

--- src/eisenstein.jl
+++ src/eisenstein.jl
@@ -1,9 +1,21 @@
 #Return the q-expansion up to precision prec (default: 10) of the weight k Eisenstein 
-#series as a polynomial over QQ in the variable var (default: "q"), normalised such that 
-#the linear coefficient is 1 
-function eisenstein_series_poly(k, prec=10, var="q")
+#series as a polynomial over K (default: QQ) in the variable var (default: "q"). 
+#Three normalizations are available: "linear" (default), "constant", and 
+#"integral". 
+#If normalization is "linear" then the linear coefficient is 1. If it is "constant"
+#then the series will be normalized to have constant term 1. If the normalization is
+#"integral" then the series will be normalized to have integer coefficients and no 
+#common factors. 
+#NOTE: The output polynomial will be over QQ if the normalization is "linear" or 
+#"constant" to prevent errors.  
+function eisenstein_series_poly(k, prec=10, K=QQ, var="q", normalization="linear")
 
-	R, q = PolynomialRing(QQ, var) 	#QQ as ring to prevent bug at the end
+	#output will be in QQ if normalization is "linear" or "constant"
+	if normalization != "integral"
+		K = QQ
+	end
+
+	R, q = PolynomialRing(K, var)
 	qexp = R(0)
 
 	#error handling 
@@ -17,12 +29,21 @@ function eisenstein_series_poly(k, prec=10, var="q")
         end
 	
 	#initialise a0
-	a0 = - bernoulli(k) // 2k 	#bernoulli is fmpq so need //
+	a0 = - bernoulli(k) // 2k 
 	
-	qexp += a0
+	qexp += a0.num
 	for n in 1:prec-1
-		qexp += sigma(fmpz(n),k-1)*q^n
+		qexp += a0.den*sigma(fmpz(n),k-1)*q^n
 	end
 
-	return qexp
+	if normalization == "linear" 
+		return qexp*1//a0.den
+	elseif normalization == "constant"
+		return qexp*1//a0.num
+	elseif normalization == "integral"
+		return qexp
+	else
+		error("normalization must be one of 'linear', 'constant', 'integral'")
+	end
+
 end
